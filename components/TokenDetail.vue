8b9ba36936b17ec76ea876940b9fe5e86ce1d2c2
Viki Val <viktorko99@gmail.com>
2022-11-10 09:37:29 +0100

    :zap: tokenDetail

---
+++ components/TokenDetail.vue
@@ -0,0 +1,20 @@
+<template>
+  <div m="auto">
+    <img  width="300" src="/kodragon.png">
+    <h3 border="~ gray-400 opacity-50" w="75" p="2">{{ name }}</h3>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const props = defineProps({
+  id: {
+    default: '',
+  },
+})
+
+const name = ref('KoDragon')
+</script>
+
+
