8a5c876ea20ac67a9b6735e4e629e11ad247032e
Viki Val <viktorko99@gmail.com>
2022-11-10 09:52:37 +0100

    :nail_care: WindiCSS is sooo good

--- components/TokenDetail.vue
+++ components/TokenDetail.vue
@@ -2,19 +2,27 @@
   <div m="auto">
     <img  width="300" src="/kodragon.png">
     <h3 border="~ gray-400 opacity-50" w="75" p="2">{{ name }}</h3>
+    <div class="text-ellipsis overflow-hidden"  border="~ gray-400 opacity-50" w="75" p="2">creator: {{ issuer }}</div>
   </div>
 </template>
 
-<script setup lang="ts">
+<script setup async lang="ts">
 import { ref } from 'vue'
+import { ask } from '@kodadot1/uniquery'
 
 const props = defineProps({
   id: {
-    default: '',
+    default: '1825819407-1',
   },
 })
 
-const name = ref('KoDragon')
+const name = ref('KoDragons#1')
+
+const res: any = await ask<any>(`/bsx/nft/${props.id}`)
+
+console.log(res.data.nft)
+
+const issuer = ref(res.data.nft.issuer)
 </script>
 
 
